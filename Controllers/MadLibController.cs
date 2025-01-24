ca51106f7efedc8032f243092e62a06c9511248b
wraithio <aarmon5@gmail.com>
2025-01-24 12:29:27 -0800

    missed a space

--- Controllers/MadLibController.cs
+++ Controllers/MadLibController.cs
@@ -18,7 +18,7 @@ namespace ARobinsonP2All4OneAPI.Controllers
         }
 
         [HttpGet]
-        [Route("goMadLib/{color}/{adjective}/{place}/{adjective_2}/{place_2}/{food_S}/ {food_P}/{verb}/{noun_P}/{adjective_3}")]
+        [Route("goMadLib/{color}/{adjective}/{place}/{adjective_2}/{place_2}/{food_S}/{food_P}/{verb}/{noun_P}/{adjective_3}")]
 
         public string goMadLib(string color, string adjective, string place, string adjective_2, string place_2, string food_S, string food_P, string verb, string noun_P, string adjective_3)
         {
